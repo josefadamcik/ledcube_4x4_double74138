efdcbe03fc72c3a928fa34f457e64240b1b9f9d4
Josef Adamcik <josef.adamcik@gmail.com>
2018-05-05 18:19:46 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,94 @@
+#include <Arduino.h>
+
+
+#define PIN_A 8
+#define PIN_B 9
+#define PIN_C 10
+#define PIN_S 11
+
+#define PIN_L0 4
+#define PIN_L1 5
+#define PIN_L2 6
+#define PIN_L3 7
+
+
+int lastLevel = -1;
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(PIN_A, OUTPUT);
+    pinMode(PIN_B, OUTPUT);
+    pinMode(PIN_C, OUTPUT);
+    pinMode(PIN_S, OUTPUT);
+    pinMode(PIN_L0, OUTPUT);
+    pinMode(PIN_L1, OUTPUT);
+    pinMode(PIN_L2, OUTPUT);
+    pinMode(PIN_L3, OUTPUT);
+
+    digitalWrite(PIN_A, LOW);
+    digitalWrite(PIN_B, LOW);
+    digitalWrite(PIN_C, LOW);
+    digitalWrite(PIN_S, HIGH);
+
+    digitalWrite(PIN_L0, LOW);
+    digitalWrite(PIN_L1, LOW);
+    digitalWrite(PIN_L1, LOW);
+    digitalWrite(PIN_L1, LOW);
+    //Serial.begin(9600);
+
+}
+
+/**
+ * @param column -  0-15
+ * @param level - 0-3
+ */ 
+void writeLed(int column, int level) {
+        if (lastLevel != level) {
+            if (lastLevel >= 0) {
+                digitalWrite(PIN_L0 + lastLevel, LOW);
+            }
+            digitalWrite(PIN_L0 + level, HIGH);
+            lastLevel = level;
+        }
+        // Serial.print(column);
+        
+        //select first or second 74138 ic
+        if (column >= 8) {
+            column -= 8;
+            digitalWrite(PIN_S, LOW);
+        } else {
+            digitalWrite(PIN_S, HIGH);
+        }
+        // Serial.print('\t');
+        // Serial.print(bitRead(column, 0));
+        // Serial.print(bitRead(column, 1));
+        // Serial.print(bitRead(column, 2));
+        digitalWrite(PIN_A, bitRead(column, 0));
+        digitalWrite(PIN_B, bitRead(column, 1));
+        digitalWrite(PIN_C, bitRead(column, 2));
+        // Serial.println();
+
+
+}
+
+void lightLevel(int l) {
+    for (byte i = 0; i < 16; i++) {
+        writeLed(i, l);
+        delay(1);
+    }
+}
+
+void loop() {
+    for (byte l = 0; l < 4; l++) {
+        for (int i = 0; i < 10; i++) {
+            lightLevel(l);
+        }
+    }
+
+    // writeLed(3,3);
+    // delay(1);
+    // writeLed(2,2);
+    // delay(1);
+    //writeLed(1,1);
+    
+}
